ef0f01fcf98be7786ec202e649c6eb83f7532161
sekarmayang05 <mayangkumala05@gmail.com>
2026-08-19 15:47:46 +0700

    RENAME

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,8 @@ const router = express.Router();
 const komikController = require('../controller/komikController');
 const userController = require('../controller/userController');
 const genreController = require('../controller/genreController');
-const authMiddleware = require('../middleware/authMiddleware');
+const authMiddleware = require('../Middleware/authMiddleware');
+
 
 
 
