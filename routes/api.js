6ed6e36918a281115875ac866e8f60f7574c43dc
sekarmayang05 <mayangkumala05@gmail.com>
2026-08-19 15:18:31 +0700

    fix: update authMiddleware import path capitalization

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const komikController = require('../controller/komikController');
-const userController = require('../controller/userController');
+const authMiddleware = require('../Middleware/authMiddleware');
 const genreController = require('../controller/genreController');
 const authMiddleware = require('../middleware/authMiddleware');
 
